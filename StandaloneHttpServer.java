d5cfbc96f7409dc40bb7ca80dba228dfa6e74b52
vs-muralidhar <vivin.murali@gmail.com>
2022-05-18 22:39:19 -0700

    refactored

--- StandaloneHttpServer.java
+++ StandaloneHttpServer.java
@@ -1,4 +1,5 @@
 import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -9,28 +10,29 @@ public class StandaloneHttpServer {
 
     static final String TRACE_ID = "x-vs-trace-id";
 
+    static HttpHandler apiHelloGETHandler = (exchange) -> {
+        exchange.getResponseHeaders().set(TRACE_ID, getTraceId(exchange.getRequestHeaders()));
+
+        if ("GET".equals(exchange.getRequestMethod())) {
+            String responseText = "Hello World!";
+            exchange.sendResponseHeaders(200, responseText.getBytes().length);
+            exchange.getResponseBody().write(responseText.getBytes());
+            exchange.getResponseBody().flush();
+        } else {
+            exchange.sendResponseHeaders(405, -1);
+        }
+        exchange.close();
+    };
+
     public static void main(String[] args) throws IOException {
-
         HttpServer server = HttpServer.create(new InetSocketAddress(6880), 0);
-        server.createContext("/api/hello", (exchange -> {
-
-            Headers requestHeaders = exchange.getRequestHeaders(); // Headers is a Map<String, List<String>>
-            String traceId = (null != requestHeaders.getFirst(TRACE_ID)) ? requestHeaders.getFirst(TRACE_ID) : UUID.randomUUID().toString();
-
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String responseText = "Hello World!";
-                exchange.getResponseHeaders().set(TRACE_ID, traceId);
-                exchange.sendResponseHeaders(200, responseText.getBytes().length);
-                exchange.getResponseBody().write(responseText.getBytes());
-                exchange.getResponseBody().flush();
-            } else {
-                exchange.getResponseHeaders().set(TRACE_ID, traceId);
-                exchange.sendResponseHeaders(405, -1);
-            }
-            exchange.close();
-        }));
-
+        server.createContext("/api/hello", apiHelloGETHandler);
         server.setExecutor(null);
         server.start();
+    }
+
+    private static String getTraceId(Headers requestHeaders) {
+        // Headers is a Map<String, List<String>>
+        return (null != requestHeaders.getFirst(TRACE_ID)) ? requestHeaders.getFirst(TRACE_ID) : UUID.randomUUID().toString();
     }
 }
