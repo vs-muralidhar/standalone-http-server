7fd4522c13b487c384e462b08b29ce5142679304
vs-muralidhar <vivin.murali@gmail.com>
2022-05-19 13:53:32 -0700

    Minor refactoring

--- StandaloneHttpServer.java
+++ StandaloneHttpServer.java
@@ -10,23 +10,33 @@ public class StandaloneHttpServer {
 
     static final String TRACE_ID = "x-vs-trace-id";
 
-    static HttpHandler apiHelloGETHandler = (exchange) -> {
+    static HttpHandler apiHelloHandler = (exchange) -> {
         exchange.getResponseHeaders().set(TRACE_ID, getTraceId(exchange.getRequestHeaders()));
+        exchange.getResponseHeaders().set("Content-type", "application/json");
+        String responseTextForGET = "Hello World GET!", responseTextForPOST = "{\"name\":\"fnu\"}";
 
-        if ("GET".equals(exchange.getRequestMethod())) {
-            String responseText = "Hello World!";
-            exchange.sendResponseHeaders(200, responseText.getBytes().length);
-            exchange.getResponseBody().write(responseText.getBytes());
-            exchange.getResponseBody().flush();
-        } else {
-            exchange.sendResponseHeaders(405, -1);
+        switch (exchange.getRequestMethod()) {
+            case "GET":
+                exchange.sendResponseHeaders(200, responseTextForGET.getBytes().length);
+                exchange.getResponseBody().write(responseTextForGET.getBytes());
+                exchange.getResponseBody().flush();
+                break;
+            case "POST":
+                exchange.sendResponseHeaders(200, responseTextForPOST.getBytes().length);
+                exchange.getResponseBody().write(responseTextForPOST.getBytes());
+                exchange.getResponseBody().flush();
+                break;
+            default:
+                exchange.sendResponseHeaders(405, -1);
+                break;
         }
+
         exchange.close();
     };
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(6880), 0);
-        server.createContext("/api/hello", apiHelloGETHandler);
+        server.createContext("/api/hello", apiHelloHandler);
         server.setExecutor(null);
         server.start();
     }
